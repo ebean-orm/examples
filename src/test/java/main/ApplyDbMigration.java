682f4d98c10fa6c30fe01198c2b3be3084189d6b
Robin Bygrave <robin.bygrave@gmail.com>
2016-05-03 11:33:14 +1200

    Add comments to ApplyDbMigration

--- src/test/java/main/ApplyDbMigration.java
+++ src/test/java/main/ApplyDbMigration.java
@@ -7,8 +7,11 @@ public class ApplyDbMigration {
 
   public static void main(String[] args) {
 
+    // ignore test-ebean.properties
     System.setProperty("disableTestProperties", "true");
 
+    // starting EbeanServer triggers the apply of migrations
+    // ... when ebean.migration.run=true
     Ebean.getDefaultServer();
 
     System.out.println("done");
